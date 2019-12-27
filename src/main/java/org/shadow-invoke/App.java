6e53b0d765d49f454e97c41bdbb70789a54a0f39
Jason DeMorrow <jason@jasondemorrow.com>
2019-12-27 13:53:54 -0800

    initial commit

---
+++ src/main/java/org/shadow-invoke/App.java
@@ -0,0 +1,13 @@
+package org.shadow-invoke;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
