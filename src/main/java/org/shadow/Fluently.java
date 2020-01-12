539f4f2e32f511718ed2f652960bdb5771fe4110
Jason DeMorrow <jason@jasondemorrow.com>
2020-01-11 17:34:12 -0800

    added named() and annotated() predicates

--- src/main/java/org/shadow/Fluently.java
+++ src/main/java/org/shadow/Fluently.java
@@ -6,7 +6,11 @@ import org.shadow.schedule.PercentageSchedule;
 import org.shadow.schedule.Schedule;
 import org.shadow.schedule.TimeSchedule;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
 
 public class Fluently {
     private Fluently() {}
@@ -25,6 +29,21 @@ public class Fluently {
 
     public static Schedule every(TimeUnit timeUnit) {
         return every(1L, timeUnit);
+    }
+
+    public static Predicate<Field> named(String... names) {
+        // TODO: Make more efficient using actual class and hashset.
+        return field -> {
+            if(field == null || names == null || names.length == 0) return false;
+            return Arrays.stream(names).anyMatch(field.getName()::equals);
+        };
+    }
+
+    public static Predicate<Field> annotated(Class<? extends Annotation> cls) {
+        return field -> {
+            if(field == null || cls == null) return false;
+            return field.getDeclaredAnnotation(cls) == null;
+        };
     }
 
     public static Filter.Builder noise() {
