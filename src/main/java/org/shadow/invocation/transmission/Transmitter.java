a5a4965d89d27c7e04c52b7159ed918de66db519
Jason DeMorrow <jason@jasondemorrow.com>
2020-01-25 07:30:17 -0800

    WIP: move to reactive streams for transmitting

--- src/main/java/org/shadow/invocation/transmission/Transmitter.java
+++ src/main/java/org/shadow/invocation/transmission/Transmitter.java
@@ -1,9 +1,69 @@
 package org.shadow.invocation.transmission;
 
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.shadow.invocation.Recording;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public interface Transmitter {
-    public void transmit(Collection<Recording> recordings) throws TransmissionException;
+@Slf4j
+public abstract class Transmitter implements Subscriber<Recording> {
+    public static final int DEFAULT_BATCH_SIZE = 10;
+    private static final int NUM_CORES = Runtime.getRuntime().availableProcessors();
+    private static final ExecutorService THREAD_POOL = Executors.newFixedThreadPool(NUM_CORES);
+    private List<Recording> pending = new ArrayList<>(DEFAULT_BATCH_SIZE);
+    private Subscription subscription = null;
+    private int batchSize = DEFAULT_BATCH_SIZE;
+
+    public abstract void transmit(Collection<Recording> recordings) throws TransmissionException;
+
+    private void sendPending(int threshold) {
+        if(this.pending.size() > threshold) {
+            try {
+                this.transmit(this.pending);
+            } catch (TransmissionException e) {
+                this.onError(e);
+                log.error(String.format("Failed to transmit %s", this.pending));
+            } finally {
+                this.pending.clear();
+            }
+        }
+        if(this.subscription != null) {
+            this.subscription.request(1L);
+        }
+    }
+
+    public Transmitter withBatchSize(int size) {
+        this.batchSize = size;
+        sendPending(0);
+        this.pending = new ArrayList<>(size);
+        return this;
+    }
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        this.subscription = subscription;
+        sendPending(0);
+    }
+
+    @Override
+    public void onNext(Recording recording) {
+        this.sendPending(this.batchSize);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        log.error(this.getClass().getCanonicalName(), throwable);
+    }
+
+    @Override
+    public void onComplete() {
+        this.subscription = null;
+        sendPending(0);
+    }
 }
